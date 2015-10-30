e39ffd25d9a0c7390db3198fd9cb6e79d04cf58a
Nicholas Long <nicholas.long@nrel.gov>
2015-10-30 13:09:22 -0600

    cleanup

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Start MongoDB
+/sbin/start-mongodb.sh
+
+export CI=true
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+# install dependencies and run default rake task
+cd /var/simdata/openstudio
 echo "I am running"
